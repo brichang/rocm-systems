5a17680e2c6772389e55c47535d093167be72e22
Danylo Lytovchenko <danylo.lytovchenko@amd.com>
2026-06-02 03:38:37 -0400

    Fix APU memmap test compilation on nvidia

--- projects/hip-tests/catch/unit/virtualMemoryManagement/hipMemMap.cc
+++ projects/hip-tests/catch/unit/virtualMemoryManagement/hipMemMap.cc
@@ -751,9 +751,9 @@ HIP_TEST_CASE(Unit_hipMemMap_Positive_APU_LargeAllocSpill) {
   HIP_CHECK(hipMemAddressReserve(&va, size, 0, nullptr, 0));
   REQUIRE(va != nullptr);
 
-  hipMemGenericAllocationHandle_t handle = nullptr;
+  hipMemGenericAllocationHandle_t handle = reinterpret_cast<hipMemGenericAllocationHandle_t>(nullptr);
   HIP_CHECK(hipMemCreate(&handle, size, &aprop, 0));
-  REQUIRE(handle != nullptr);
+  REQUIRE(handle != reinterpret_cast<hipMemGenericAllocationHandle_t>(nullptr));
 
   HIP_CHECK(hipMemMap(va, size, 0, handle, 0));
 
